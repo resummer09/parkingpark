71cd0f0ce8ee1207fb599ca3f9511dba9e67ee48
ElisaBluebell <selfishracer@hotmail.com>
2023-05-08 10:46:23 +0900

    yeah

--- cut_training_image.py
+++ cut_training_image.py
@@ -1,8 +1,18 @@
+import time
+
 import cv2
 import os
 
 
-cv2.bootstrap()
+def vehicle_coordinate(full_filename):
+    filename = os.path.splitext(full_filename)[0]
+
+
+def cut_training_image(full_filename):
+    image = cv2.imread("1_20200713_153849_000030.jpg", cv2.IMREAD_COLOR)
+    cv2.imshow("hey", image)
+    print(full_filename)
+    time.sleep(5)
 
 def search(dirname):
     try:
@@ -12,7 +22,8 @@ def search(dirname):
             full_filename = os.path.join(dirname, filename)
 
             if os.path.splitext(full_filename)[1] == ".jpg":
-                pass
+                # 파일 잘라내기 작업
+                cut_training_image(full_filename)
 
             elif os.path.isdir(full_filename):
                 search(full_filename)
